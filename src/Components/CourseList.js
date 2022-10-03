a210bb450931b9a04eec2483a7517b1e9dedfe8e
Railgun707 <73008040+Railgun707@users.noreply.github.com>
2022-10-03 12:52:56 -0500

    update

--- src/Components/CourseList.js
+++ src/Components/CourseList.js
@@ -4,7 +4,7 @@ const CourseList = (list) =>(
         {Object.entries(list).map(([id, courses]) => 
         <div className="card m-1 p-2" key = {id}> 
             <div>
-                <div className = "top"><b style = {{fontSize: "25px"}}>{courses.term} {courses.number}</b></div>
+                <div className = "top"><b style = {{fontSize: "25px"}}>{courses.term} {" CS "} {courses.number}</b></div>
                 <div className = "middle">{courses.title}</div> 
                 <hr></hr>
                 <div className = "bottom">{courses.meets}</div>
